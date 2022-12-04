78cd759e9c2d50064f217388b41877a552a37793
unknown <affanashfaq786@gmail.com>
2022-12-04 20:28:56 +0500

    completetd

---
+++ App.js
@@ -0,0 +1,16 @@
+var image = document.getElementById('inp')
+var alpha = document.getElementById('gnp')
+var c = []    
+function abc(a){
+   var b = image
+   if(b == ""){
+ alpha.image = 'https://media.gettyimages.com/id/184944186/photo/quaid-e-azam.jpg?s=612x612&w=gi&k=20&c=Nr9cDm0BY-yx1eu7bUGN3QGk87VybswqcqTwT05S-U8='
+   }else{
+    a.push(c);
+    console.log(c)
+  
+   }
+    // console.log(a);
+}
+// a.src = 'https://media.gettyimages.com/id/184944186/photo/quaid-e-azam.jpg?s=612x612&w=gi&k=20&c=Nr9cDm0BY-yx1eu7bUGN3QGk87VybswqcqTwT05S-U8='
+// a.src = "https://media.istockphoto.com/id/1211781372/photo/sityscape-of-beautiful-metropolitan-city-karachi.jpg?s=612x612&w=0&k=20&c=s8x-7guoOpjclH2gnW1Eh6hE9Zj54YnIIm6vfY6C3Ig="
